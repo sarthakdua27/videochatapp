1f5955c468baff657bdc99494c44ae653e7fc9ab
Nandini-4120 <78912133+Nandini-4120@users.noreply.github.com>
2021-07-13 12:55:44 +0530

    commits

--- server.js
+++ server.js
@@ -11,22 +11,15 @@ const peerServer= ExpressPeerServer(server,{
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-// app.use('/peerjs',peerServer);
-// //redirect the root url 
-// app.get('/',(req,res) =>{
-//    res.redirect( `/${uuidv4()}` )
-// })
 
 app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
    res.redirect('/login');
-   // res.redirect( `/${uuidv4()}` )
 })
 app.get('/login', (req, res) => {
    res.render('login');
 })
 app.get('/room', (req, res) => {
-   // res.render('room');
    res.redirect(`/${uuidv4()}`)
 })
 
@@ -35,39 +28,18 @@ app.get('/:room',(req,res)=>{
 
 })
 
-let onlinelist=[];
+let onlinelist=[];//list of online participants
 io.on('connection', socket => {
 
    socket.on("onuserconnected", function (username) {
       let userobj = { "id": socket.id, "username": username };
       onlinelist.push(userobj);
-      // console.log(onlinelist);
       // update my online list with userlist
       socket.emit("updatemylist", onlinelist);
       //broadcast it to all other devices=> emit
       socket.broadcast.emit("joinedthechat",userobj);
    });
 
-// io.on('connection',socket =>{
-//    socket.on('join-room',(UniqueRoomId,userId)=>{
-//       socket.join(UniqueRoomId);
-//       socket.broadcast.to(UniqueRoomId).emit('user-connected',userId);
-//       socket.on('disconnect', () => {
-//          socket.broadcast.to(UniqueRoomId).emit('user-disconnected', userId)
-//       })
-//       socket.on('message',obj=>{     //sending the message here
-//          io.to(UniqueRoomId).emit('MsgCreation',obj)
-//       })
-//       /*socket.on('message',message=>{     //sending the message here
-//          io.to(UniqueRoomId).emit('MsgCreation',message)
-//       })*/
-//    })
-//    /*socket.on("disconnect",function(){
-//       //document.querySelector(`.${socket.id}`).remove();
-//       socket.emit("remove",socket.id);*/
-   
-
-// })
 socket.on('join-room', (UniqueRoomId, userId) => {
    socket.join(UniqueRoomId);
    socket.broadcast.to(UniqueRoomId).emit('user-connected', userId);
@@ -89,14 +61,7 @@ socket.on('join-room', (UniqueRoomId, userId) => {
    socket.on("chat",function(obj){
       socket.broadcast.emit("uploadleftmsg",obj);
   })
-   /*socket.on('message',message=>{     //sending the message here
-      io.to(UniqueRoomId).emit('MsgCreation',message)
-   })*/
 })
-/*socket.on("disconnect",function(){
-   //document.querySelector(`.${socket.id}`).remove();
-   socket.emit("remove",socket.id);*/
-
 
 })
 
